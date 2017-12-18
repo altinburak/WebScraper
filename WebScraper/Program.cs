5f2ade7c4e5cea8fc59f77132005509f8321a4e4
Burak Altin <burak@officehq.com.au>
2017-12-18 16:58:06 +1100

    Initial commit

--- WebScraper/Program.cs
+++ WebScraper/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
+using System.Net;
 
 namespace WebScraper
 {
@@ -10,6 +7,63 @@ namespace WebScraper
 	{
 		static void Main(string[] args)
 		{
+			StartTheProcess();
 		}
+
+		private static void StartTheProcess()
+		{
+			var file = new StreamReader(@"C:\Users\Burak\Desktop\loesdau.de_for_scraping_-_Sheet0.txt");
+			string line;
+			while ((line = file.ReadLine()) != null)
+			{
+				var html = HttpGet(line);
+				var name = GetName(html);
+				var description = GetDescription(html);
+				var images = GetImages(html);
+
+			}
+			file.Close();
+
+		}
+
+		private static string GetName(string html)
+		{
+			var startIndex = html.IndexOf("<h2 class=\"subline js_only\">");
+			startIndex = html.IndexOf(">", startIndex);
+			var endIndex = html.IndexOf("</h2>", startIndex);
+			return html.Substring(startIndex + 1, endIndex - startIndex);
+		}
+
+		private static string GetDescription(string html)
+		{
+			var startIndex = html.IndexOf("<div itemprop=\"description\"");
+			startIndex = html.IndexOf(">", startIndex);
+			var endIndex = html.IndexOf("</div>", startIndex);
+			return html.Substring(startIndex + 1, endIndex - startIndex);
+		}
+
+		private static string GetImages(string html)
+		{
+			var startIndex = html.IndexOf("<div id=\"product-thumbnails\">");
+			startIndex = html.IndexOf(">", startIndex);
+			var endIndex = html.IndexOf("</div>", startIndex);
+			return html.Substring(startIndex + 1, endIndex - startIndex);
+		}
+
+		private static string GetVariants(string html)
+		{
+			var startIndex = html.IndexOf("<div class=\"dropDown\">");
+			startIndex = html.IndexOf(">", startIndex);
+			var endIndex = html.IndexOf("</div>", startIndex);
+			return html.Substring(startIndex + 1, endIndex - startIndex);
+		}
+		public static string HttpGet(string URI)
+		{
+			var req = WebRequest.Create(URI);
+			var resp = req.GetResponse();
+			var sr = new StreamReader(resp.GetResponseStream());
+			return sr.ReadToEnd().Trim();
+		}
+
 	}
 }
